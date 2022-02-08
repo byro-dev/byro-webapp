63802014c43cb7f983c7a6e99cc3f74e3262d253
Bereket Sega <bereketsega8@gmail.com>
2022-02-08 01:28:57 -0500

    added login and signup auth

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Api running");
+});
+
+// Connecting Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+// Error Handler Middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
 
